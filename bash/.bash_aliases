d03484bad79cdc61550a4611d208326e55fd597e
Justin-p <justin-p@users.noreply.github.com>
2020-07-27 15:25:53 +0200

    Update

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,3 +1,4 @@
+
 ii() {
     nohup nautilus -w $1 > /dev/null 2>&1 &
 }
---
+++ fzf/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/justin-p/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/justin-p/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# [[ $- == *i* ]] && source "/home/justin-p/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+# source "/home/justin-p/.fzf/shell/key-bindings.zsh"
