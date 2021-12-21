bb7b224c284bec9c74edbdac1f5704fd321dd0e4
Justin-p <justin-p@users.noreply.github.com>
2021-12-21 17:32:16 +0100

    cleanup

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -4,14 +4,13 @@ If (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 $ParentProcess = (gwmi win32_process -Filter "processid='$((gwmi win32_process -Filter "processid='$pid'").Parentprocessid)'").Name
-If ( $ParentProcess -eq 'WindowsTerminal.exe' -or $ParentProcess -eq 'code.exe' -or $ParentProcess -eq 'ConEmuC64.exe') {
+If ( $ParentProcess -eq 'WindowsTerminal.exe' -or $ParentProcess -eq 'code.exe') {
     $LoadTheme = $True
 }
 # Modules
 $Modules = @( 
-    "posh-git",    
+    "posh-git",
     "oh-my-posh",
-    "windows-screenfetch",
     "Get-ChildItemColor"
 )
 ForEach ($Module in $Modules) { 
@@ -21,13 +20,7 @@ ForEach ($Module in $Modules) {
             If ($Module -eq 'oh-my-posh') {
                 Set-Theme Paradox
             }
-            If ($Module -eq 'windows-screenfetch') {
-                $OG = $ErrorActionPreference
-                $ErrorActionPreference = 'SilentlyContinue' 
-                Screenfetch
-                $ErrorActionPreference = $OG
-            }
-        }    
+        }
         If ($Module -eq 'Get-ChildItemColor') {
             Set-Alias ls Get-ChildItemColor -option AllScope
         }
